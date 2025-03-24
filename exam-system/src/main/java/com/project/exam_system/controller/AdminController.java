bedbe7b6722027800d174489d800184d4624a5d2
feather02 <sribinufeather@gmail.com>
2025-03-24 15:31:18 +0530

    updates on 24/03/2025

--- exam-system/src/main/java/com/project/exam_system/controller/AdminController.java
+++ exam-system/src/main/java/com/project/exam_system/controller/AdminController.java
@@ -6,6 +6,8 @@ import com.project.exam_system.service.AdminService;
 import com.project.exam_system.service.FacultyService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -104,5 +106,4 @@ public class AdminController {
 //        adminService.registerAdmin(admin);
 //        return "Admin Registered successfully";
 //    }
-
 }
